c01465b443f04d02813c9d69cdcdbf5cf13c12ef
ljingr <ljingr@vt.edu>
2023-08-23 22:20:19 -0400

    Add car make and car model

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-# from .restapis import related methods
+from .restapis import get_dealer_reviews_from_cf, get_dealers_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -83,7 +83,16 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
+        context["dealerships"] = get_dealers_from_cf()
+
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/a2c9bdd2-c454-4b7d-bb66-97af8efc34cf/default/get-dealership"
+        dealerships = get_dealers_from_cf(url)
+        context["dealerships"] = dealerships
+        states = [dealer.st for dealer in dealerships]
+        unique_states = list(set(states))
+        context["unique_states"] = unique_states
         return render(request, 'djangoapp/index.html', context)
+        
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
